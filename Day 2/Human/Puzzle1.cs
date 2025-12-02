6bc6f64b128574c289932cb6fa27c88ee4da8094
Paul <1758965+paulbreen@users.noreply.github.com>
2025-12-02 11:55:12 +0000

    Day 2, Human solution, plus instructions for AI

---
+++ Day 2/Human/Puzzle1.cs
@@ -0,0 +1,50 @@
+﻿namespace Human
+{
+    public static class Puzzle1
+    {
+
+        public static Int64 Solve(string fileInput)
+        {
+            var list = fileInput.Split(',');
+
+            Int64 total = 0;
+
+            foreach (var item in list)
+            {
+                var range = item.Split("-");
+
+                var startingRange = Int64.Parse(range[0]);
+                var endingRange = Int64.Parse(range[1]);
+
+                for (Int64 i = startingRange; i <= endingRange; i++)
+                {
+                    if (!Validate(i))
+                    {
+                        total += i;
+                    }
+                }
+            }
+
+            return total;
+        }
+
+
+        private static bool Validate(Int64 id)
+        {
+            var stringId = id.ToString();
+
+            // rule 1, has odd number of digits
+            if ((stringId.Length % 2) == 1) return true;
+
+
+            //rule 2, repeated twice
+            var halfLength = (stringId.Length / 2);
+            var firstHalf = stringId.Substring(0, halfLength);
+            var secondHalf = stringId.Substring(halfLength);
+
+            if (firstHalf == secondHalf) return false;
+
+            return true;
+        }
+    }
+}
---
+++ Day 2/Human/Puzzle2.cs
@@ -0,0 +1,57 @@
+﻿using System.Data;
+using System.Diagnostics;
+
+namespace Human
+{
+    public static class Puzzle2
+    {
+
+        public static Int64 Solve(string fileInput)
+        {
+            var list = fileInput.Split(',');
+
+            Int64 total = 0;
+
+            foreach (var item in list)
+            {
+                var range = item.Split("-");
+
+                var startingRange = Int64.Parse(range[0]);
+                var endingRange = Int64.Parse(range[1]);
+
+                for (Int64 i = startingRange; i <= endingRange; i++)
+                {
+                    if (!Validate(i))
+                    {
+                        total += i;
+                    }
+                }
+            }
+
+            return total;
+        }
+
+
+        private static bool Validate(Int64 id)
+        {
+            var stringId = id.ToString();
+            var halfLength = (stringId.Length / 2);
+
+            // rule 
+            // ID is invalid if it is made only of some sequence of digits repeated at least twice
+            for (int i = 1; i <= halfLength; i++)
+            {
+                // split string into increasing length of chunks
+                var chunk = stringId.Chunk(i).Select (x => new string(x)).ToList();
+
+                // group the chunks
+                var groupChunk = chunk.GroupBy(x => x);
+                
+                //if there is more than one group, we know there is more than one sequence.
+                if(groupChunk.Count() == 1) return false;
+            }
+
+            return true;
+        }
+    }
+}
