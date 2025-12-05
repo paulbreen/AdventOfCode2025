4d96f901c68e95265937a47b55c84de93c8eee60
Paul <1758965+paulbreen@users.noreply.github.com>
2025-12-05 12:45:13 +0000

    Day 5 Human

---
+++ Day 5/Human/Puzzle1.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Human
+{
+    public static class Puzzle1
+    {
+        public static int Solve(string[] fileInput)
+        {
+            var ranges = new List<(Int64 min, Int64 max)>();
+            var count = 0;
+
+            for (int i = 0; i < fileInput.Length; i++)
+            {
+                if (string.IsNullOrEmpty(fileInput[i])) continue;
+
+                if (fileInput[i].Contains('-'))
+                {
+                    var s = fileInput[i].Split('-');
+
+                    ranges.Add((Int64.Parse(s[0]), Int64.Parse(s[1])));
+
+                    continue;
+                }
+
+                var id = Int64.Parse(fileInput[i]);
+
+                if(ranges.Any(x => x.min <= id && x.max >= id)) count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Day 5/Human/Puzzle2.cs
@@ -0,0 +1,51 @@
+﻿namespace Human
+{
+    public static class Puzzle2
+    {
+        public static long Solve(string[] fileInput)
+        {
+            long total = 0;
+
+            var ranges  = new List<(long min, long max)>();
+
+            for (int i = 0; i < fileInput.Length; i++)
+            {
+                if (string.IsNullOrEmpty(fileInput[i])) continue;
+
+                if (fileInput[i].Contains('-'))
+                {
+                    var s = fileInput[i].Split('-');
+
+                    ranges.Add((long.Parse(s[0]), long.Parse(s[1])));
+
+                    continue;
+                }
+            }
+
+            var sortedRanges  = ranges.OrderBy(x => x.min).ToArray();
+
+            long currentMin = sortedRanges[0].min;
+            long currentMax = sortedRanges[0].max;
+
+            for(int i = 1; i < sortedRanges.Count(); i++)
+            {
+                var range = sortedRanges[i];
+
+                if (range.min <= currentMax + 1)
+                {
+                    currentMax = Math.Max(currentMax, range.max);
+                }
+                else
+                {
+                    total += (currentMax - currentMin + 1);
+                    currentMin = range.min;
+                    currentMax = range.max;
+                }
+            }
+
+            total += (currentMax - currentMin + 1);  // last one
+
+            return total;
+        }
+    }
+}
