7d08c0a896f24714fd61473f2ceeb0f66b1f7587
Paul <1758965+paulbreen@users.noreply.github.com>
2025-12-08 13:34:22 +0000

    Human Day 7

---
+++ Day 7/Human/Puzzle1.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Human
+{
+    public static class Puzzle1
+    {
+        public static Int64 Solve(string[] fileInput)
+        {
+            var total = 0;
+
+            var tachyon = new char[fileInput.Length][];
+
+            for (int i = 0; i < fileInput.Length; i++)
+            {
+                tachyon[i] = fileInput[i].ToArray();
+            }
+
+            var startPosition = fileInput[0].IndexOf('S');
+
+            for (int i = 0; i < tachyon.Length; i++)
+            {
+                if(i == 1)
+                {
+                    tachyon[i][startPosition] = '|';
+                }
+
+
+                for (var j = 0; j < tachyon[i].Length; j++)
+                {
+                    if (i == 0) break;
+
+                    //check position directly above
+                    if (tachyon[i - 1][j] == '|'){
+
+                        //if we are a splitter, then split
+                        if (tachyon[i][j] == '^')
+                        {
+                            tachyon[i][j - 1] = '|';
+                            tachyon[i][j + 1] = '|';
+                            total++;
+                        }
+                        else
+                        {
+                            //change to beam
+                            tachyon[i][j] = '|';
+                        }
+                    }
+                }
+
+                //Print(tachyon);
+            }
+
+            return total;
+        }
+
+
+        private static void Print(char[][] tachyon)
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < tachyon.Length; i++)
+            {
+                for (var j = 0; j < tachyon[i].Length; j++)
+                {
+                    Console.Write(tachyon[i][j]);
+
+                    Thread.Sleep(1);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Day 7/Human/Puzzle2.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Human
+{
+    public static class Puzzle2
+    {
+        public static Int64 Solve(string[] fileInput)
+        {
+            var tachyon = new char[fileInput.Length][];
+
+            for (int i = 0; i < fileInput.Length; i++)
+            {
+                tachyon[i] = fileInput[i].ToArray();
+            }
+
+            var startPosition = fileInput[0].IndexOf('S');
+
+            var cache = new Dictionary<(int row, int col), long>();
+
+            return CountTimelines(tachyon, 1, startPosition, cache);
+        }
+
+
+        private static long CountTimelines(char[][] manifold, int row, int col, Dictionary<(int, int), long> cache)
+        {
+            // reached the bottom
+            if (row >= manifold.Length)
+                return 1;
+
+            // out of bounds
+            if (col < 0 || col >= manifold[row].Length)
+                return 0;
+
+            // pre calculated
+            if (cache.ContainsKey((row, col)))
+                return cache[(row, col)];
+
+            long result;
+
+            // is splitter
+            if (manifold[row][col] == '^')
+            {
+                long leftTimelines = CountTimelines(manifold, row + 1, col - 1, cache);
+                long rightTimelines = CountTimelines(manifold, row + 1, col + 1, cache);
+                result = leftTimelines + rightTimelines;
+            }
+            else
+            {
+                // Continue straight down
+                result = CountTimelines(manifold, row + 1, col, cache);
+            }
+
+            // Cache the result
+            cache[(row, col)] = result;
+
+            return result;
+        }
+    }
+}
+
